ac3fc343c2673a38eb24874d4feecebabb90822a
Gizem_unal <gizem.unal@anywai.ch>
2023-12-27 15:53:52 +0300

    test: add country test

---
+++ cypress/e2e/country.cy.ts
@@ -0,0 +1,13 @@
+describe("Get Countries", () => {
+  it("should see all countries", () => {
+    cy.visit("http://localhost:3000/");
+
+    cy.get('[data-cy="getCountries"]').click();
+
+    cy.get("ul").find("li").should("have.length.greaterThan", 0);
+
+    cy.get("ul").find("li").contains("Ankara").should("exist");
+
+    cy.get("ul").find("li").contains("Samsun").should("not.exist");
+  });
+});
